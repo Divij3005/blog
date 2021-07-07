aba59946b51f01ad5843e5989f0e1cd27465d1d4
Pranay Bhatia <pranaybhatia707@gmail.com>
2021-07-07 16:46:57 +0530

    'First Commit'

---
+++ frontend/src/Components/Home.jsx
@@ -0,0 +1,57 @@
+import React, {Component} from "react";
+import './styles/Home.css'
+
+
+class ComposeButton extends Component{
+    
+    render(){
+        let w = window.innerWidth;
+        let cb;
+        if (w > 1180){
+            cb = <button className="compose-button"><h2> Compose <i class="fa fa-pencil" aria-hidden="true"></i> </h2></button>;
+        }
+        else {
+            cb = <button><i class="fa fa-pencil" aria-hidden="true"></i></button>;
+        }
+        return(
+            <>
+                {cb}
+            </>
+        );
+    }
+}
+
+class Posts extends Component{
+    render(){
+        return(
+            <div className="post">
+                <h1> Author </h1>
+                <div className="upper-div">
+                    <p className="date"> date </p>
+                    <p className="topic"> Topic </p>
+                </div>
+                <div className="content">
+                    <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Interdum velit laoreet id donec ultrices tincidunt arcu non sodales. Libero justo laoreet sit amet cursus sit amet dictum. Tellus mauris a diam maecenas sed enim ut sem. Fusce id velit ut tortor pretium viverra suspendisse. Urna condimentum mattis pellentesque id nibh tortor id aliquet lectus. Bibendum at varius vel pharetra. Fermentum leo vel orci porta non pulvinar. Vestibulum lorem sed risus ultricies tristique nulla aliquet enim. Blandit massa enim nec dui. Ante in nibh mauris cursus mattis. Amet venenatis urna cursus eget nunc scelerisque viverra. Sagittis vitae et leo duis ut diam quam nulla porttitor. Ac tortor dignissim convallis aenean et tortor at risus. Habitant morbi tristique senectus et netus et malesuada fames. Ultrices vitae auctor eu augue ut lectus. Commodo elit at imperdiet dui accumsan sit amet nulla. Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida.</p>
+                </div>
+                <button> Expand </button>
+            </div>
+        );
+    }
+}
+
+class Home extends Component{
+    render(){
+        return(
+            <>
+                <div className="Box"></div>
+                <ComposeButton />
+                <Posts />
+                <Posts />
+                <Posts />
+                <Posts />
+            </>
+        );
+    }
+}
+
+export default Home;
